d0be854d09ee367b2e4fbdc4f6fa968beecb5bd4
Richard <fahitch+github@gmail.com>
2019-11-19 16:24:54 +0000

    initial commit

---
+++ specs/paint_lab_spec.js
@@ -0,0 +1,25 @@
+const assert = require('assert');
+const Room = require('../models/room.js');
+const Paint = require('../models/paint.js');
+const Decorator = require('../models/decorator.js');
+
+describe('Room', function () {
+
+  it('should have an area in square meters');
+  xit('should start not painted');
+  xit('should be able to be painted');
+});
+
+describe('Paint', function () {
+  xit('should have a number of litres of paint');
+  xit('should be able to check if it is empty');
+  xit('should be able to empty itself of paint');
+});
+
+describe('Decorator', function() {
+  xit('start with an empty paint stock');
+  xit('be able to add a can of paint to paint stock');
+  xit('be able to calculate total litres paint it has in stock');
+  xit('be able to calculate whether it has enough paint to paint a room');
+  xit('be able to paint room if it has enough paint in stock');
+};
